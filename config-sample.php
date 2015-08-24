9b6ad1854a519a4f6f4ab823c76be08279e1989c
plushyObject <jamesdarrohn@gmail.com>
2015-08-24 18:02:58 -0400

    Added Root Path to config.php

--- config-sample.php
+++ config-sample.php
@@ -8,6 +8,7 @@ define('DB_HOST' , 'localhost');
 define('DB_PORT' , '3306');
 define('DB_NAME' , 'YourDBName');
 define('DB_USER' , 'YourUserName');
-define('DB_PASS' , 'YourPassWord');
+define('DB_PASS' , 'YourPassword');
+define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT']);
 
 ?>
